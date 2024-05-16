a569ad8daad0dbb19b820e0361f8bd23c1a12c77
Konyuka <michaelsaiba84@gmail.com>
2024-05-16 13:11:02 +0300

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        primary: "#e1251b",
+        secondary: "#4d4f51",
+      },
+      animation: {
+        gradient: "gradient 20s linear infinite",
+        pulse: "pulse 5s ease-in-out infinite",
+      },
+      keyframes: {
+        gradient: {
+          to: { "background-position": "200% center" },
+        },
+      },
+    },
+  },
+  plugins: [forms, typography, aspect_ratio],
+};
+
